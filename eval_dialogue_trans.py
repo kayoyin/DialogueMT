0f06dfab1b6e7c6237721cd130f22751ddc5ec7b
Kayo Yin <kayo@cmu.edu>
2020-08-30 18:01:59 -0400

    add outputs of 2+1

--- eval_dialogue_trans.py
+++ eval_dialogue_trans.py
@@ -81,59 +81,9 @@ if __name__ == "__main__":
         refs += ref
 
     with open(args.output, 'w') as file:
-        if sample['target'] is not None:
-            file.write(f"BLEU score = {sacrebleu.corpus_bleu(preds, [refs]).score}\n")
-        for h in preds:
-            file.write(f"{h}\n")
-    raise ValueError
-    while True:    
-        has_target = sample['target'] is not None
-        sample = utils.move_to_cuda(sample) if use_cuda else sample
-        if 'net_input' not in sample:
-            continue
-
-        hypos = task.inference_step(generator, [model], sample)
-        
-        for i, sample_id in enumerate(sample['id'].tolist()):
-            has_target = sample['target'] is not None
-
-            # Remove padding
-            if 'src_tokens' in sample['net_input']:
-                src_tokens = utils.strip_pad(sample['net_input']['src_tokens'][i, :], dictionary.pad())
-            else:
-                src_tokens = None
-
-            target_tokens = None
-            if has_target:
-                target_tokens = utils.strip_pad(sample['target'][i, :], dictionary.pad()).int().cpu()
-
-            # Either retrieve the original sentences or regenerate them from tokens.
-            if align_dict is not None:
-                src_str = task.dataset(args.gen_subset).src.get_original_text(sample_id)
-                target_str = task.dataset(args.gen_subset).tgt.get_original_text(sample_id)
-                refs.append(target_str)
-            else:
-                src_str = dataset.src[sample_id] # TODO: include previous sentence here for alignment??
-                if has_target:
-                    target_str = dataset.tgt[sample_id]
-                    refs.append(target_str)
-
-            # Process top predictions
-            for j, hypo in enumerate(hypos[i][:args.nbest]):
-                hypo_tokens, hypo_str, alignment = utils.post_process_prediction(
-                    hypo_tokens=hypo['tokens'].int().cpu(),
-                    src_str=src_str,
-                    alignment=hypo['alignment'],
-                    align_dict=align_dict,
-                    tgt_dict=dictionary,
-                    remove_bpe=args.remove_bpe,
-                    extra_symbols_to_ignore=get_symbols_to_strip_from_output(generator),
-                )
-                detok_hypo_str = hypo_str
-                preds.append(hypo_str)
-    with open(args.output, 'w') as file:
         if has_target:
             file.write(f"BLEU score = {sacrebleu.corpus_bleu(preds, [refs]).score}\n")
         for h,r in zip(preds, refs):
             file.write(f"P: {h}\n")
             file.write(f"T: {r}\n")
+            
